ac84823b0496ee6b254d959218d403aab1f59e21
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-08-25 17:01:13 -0700

    got first chart rendering

--- src/components/Viz.jsx
+++ src/components/Viz.jsx
@@ -1,7 +1,13 @@
 import './Viz.css';
+import * as d3 from 'd3';
+import Chart from './Chart.jsx';
 
-export default function Viz() {
+export default function Viz({data}) {
   // TODO : Visualize the data!
-
-  return <main className="viz">Viz goes here</main>;
+  return (
+  <>
+    <Chart data={data} />
+    {/* <main className="viz">Viz goes here</main> */}
+  </>
+  );
 }
