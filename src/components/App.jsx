c93d8f3d42fce083deeaef99d3a920351134b023
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-08-25 17:00:21 -0700

    edit fetch resource to add state and passed state down to viz component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,35 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import * as api from '../api';
 import ControlPanel from './ControlPanel';
 import Viz from './Viz';
 
-export default function App() {
+export default function App(props) {
+  const [data, setData] = useState(null);
   // Example of how you could fetch data
   useEffect(() => {
     async function fetchData() {
       const metadata = await api.fetchMetadata();
       const citation = await api.fetchCitation();
-      const data = await api.fetchData({
+      const newData = await api.fetchData({
         location_name: ['Belize', 'Venezuela'],
         year_name: [1990, 2015],
         sex_name: ['Males', 'Females'],
       });
-      console.log({ metadata, citation, data });
+      setData(newData);
+      console.log({ metadata, citation, newData });
     }
     fetchData();
-  }, []);
+  }, [props.id]);
 
+  if (data){
   return (
     <div className="App">
       <ControlPanel />
-      <Viz />
+      <Viz data={data}/>
     </div>
   );
+  } else {
+    return null;
+  }
 }
