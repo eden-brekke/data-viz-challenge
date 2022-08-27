66621333ea4633532ad1c6f0ed5941bba2501c6d
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-08-26 18:17:59 -0700

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Footer.css';
+
+export default function Footer(){
+  return (
+    <>
+      <div id='footer'>
+        <a href="http://github.com/eden-brekke">Developed By Eden</a>
+        <a href="http://github.com/eden-brekke">References</a>
+        <p> Data Visualization Challenge</p>
+      </div>
+    </>
+  )
+}
