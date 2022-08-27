e9721a97b52005a27513e853834d401b69d4ce92
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-08-27 10:35:23 -0700

    fixed renderings

--- src/components/Viz.jsx
+++ src/components/Viz.jsx
@@ -1,13 +1,14 @@
 import './Viz.css';
-import * as d3 from 'd3';
 import Chart from './Chart.jsx';
 import LineChart from './LineChart.jsx';
+import Chart2 from './Chart2.jsx';
 
 export default function Viz({data}) {
   // TODO : Visualize the data!
   return (
   <>
     <Chart data={data} />
+    <Chart2 data={data} />
     <LineChart data={data} />
     {/* <main className="viz">Viz goes here</main> */}
   </>
