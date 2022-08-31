7e512f34520e43aefd436573cbefd9624c20fa8d
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-08-30 21:29:07 -0700

    adjustments to rendering

--- src/components/buildingblocks/Main.jsx
+++ src/components/buildingblocks/Main.jsx
@@ -7,28 +7,34 @@ export default function Header({ data, sexMeta, locMeta, yearMeta }) {
 
   return (
     <>
-    <div>
-      <h1>Opioid Use Disorder Data</h1>
-    </div>
-    <div>
-      <h2>Data Visualization Challenge</h2>
-    </div>
-    <Router>
-      <div id='nav'>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/highrates">Highest Mortality Rates</Link>
-          <Link to="/lowrates">Lowest Mortality Rates</Link>
-          <Link to="/yearlyrates">Yearly Death Rates per Location</Link>
-        </nav>
+      <div className="main-full">
+        <div className="header">
+          <div>
+            <h1>Opioid Use Disorder</h1>
+          </div>
+          <div>
+            <h2>Data Visualization</h2>
+          </div>
+        </div>
+        <div className="nav-bar">
+          <Router>
+            <div id='nav'>
+              <nav>
+                <Link to="/">Home</Link>
+                <Link to="/highrates">Highest Mortality Rates by Year</Link>
+                <Link to="/lowrates">Lowest Mortality Rates by Year</Link>
+                <Link to="/yearlyrates">Yearly Death Rates by Location</Link>
+              </nav>
+            </div>
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/highrates" element={<ControlPanel sexMeta={sexMeta} yearMeta={yearMeta} chartType="highrates" />} />
+              <Route path="/lowrates" element={<ControlPanel sexMeta={sexMeta} yearMeta={yearMeta} chartType="lowrates" />} />
+              <Route path="/yearlyrates" element={<ControlPanel locMeta={locMeta} chartType="yearly" />} />
+            </Routes>
+          </Router>
+        </div>
       </div>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/highrates" element={<ControlPanel sexMeta={sexMeta} yearMeta={yearMeta} chartType="highrates"/>} />
-        <Route path="/lowrates" element={<ControlPanel sexMeta={sexMeta} yearMeta={yearMeta} chartType="lowrates"/>} />
-        <Route path="/yearlyrates" element={<ControlPanel locMeta={locMeta} chartType="yearly" />} />
-      </Routes>
-    </Router>
     </>
   )
 };
