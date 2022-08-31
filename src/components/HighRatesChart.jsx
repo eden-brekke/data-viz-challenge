09436647ab3dbca96750d63e006b1dfbf1ccf1dc
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-08-30 21:28:07 -0700

    added text and loading

--- src/components/HighRatesChart.jsx
+++ src/components/HighRatesChart.jsx
@@ -1,5 +1,5 @@
-import './Chart.css';
 import BarChart from './charts/vertical-bar-chart';
+import './Chart.css';
 
 export default function Chart({ data }) {
   data.sort((a, b) => b.mean - a.mean);
@@ -12,7 +12,25 @@ export default function Chart({ data }) {
     width: 800,
     color: "green"
   });
-  return (
-    <svg ref={chart}></svg>
-  );
+  if (data[0] === "Loading") {
+    return (
+      <>
+        <div class="spinner">
+          <p>loading...</p>
+          <div class="spinner-area spinner-first"></div>
+          <div class="spinner-area spinner-second"></div>
+          <div class="spinner-area spinner-third"></div>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h3 className="graph title">Top Twenty Locations with the Highest Mortality Rates within a Year</h3>
+        <svg ref={chart}></svg>
+        <p className="mean-def">Mean: This is the mean death rate (number of deaths per 100,000 people) due to opioid use disorders for a given location, sex, and year.</p>
+        <p>In the Above Graph the X Axis depicts the Location correlating to the Y axis in which the mean death rate is given.<br></br>Graphs such as this give us a good idea of which geographical locations are most and least affected by Opioid use Disorder.</p>
+      </>
+    );
+  }
 }
