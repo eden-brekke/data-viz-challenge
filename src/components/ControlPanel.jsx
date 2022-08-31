ab461174ae995ef2ff9b440dbaaae5644c6c587e
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-08-30 21:27:54 -0700

    adjustments to rendering

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -3,16 +3,17 @@ import { useState } from 'react'
 import HighRates from './HighRatesChart'
 import LowRates from './LowRatesChart'
 import Yearly from './YearlyRatesChart'
+import './ControlPanel.css'
 
 
 
 
-function ControlPanel({sexMeta, locMeta, yearMeta, chartType}) {
+function ControlPanel({ sexMeta, locMeta, yearMeta, chartType }) {
   const [data, setData] = useState(['Loading']);
-  
-  async function dataHandler(event){
-    event.preventDefault(); 
-    const location = document.getElementById('userSelectedLoc') === null ? [] : document.getElementById('userSelectedLoc').value; 
+
+  async function dataHandler(event) {
+    event.preventDefault();
+    const location = document.getElementById('userSelectedLoc') === null ? [] : document.getElementById('userSelectedLoc').value;
     const year = document.getElementById('userSelectedYear') === null ? [] : document.getElementById('userSelectedYear').value;
     const sex = document.getElementById('userSelectedSex') === null ? [] : document.getElementById('userSelectedSex').value;
     console.log("location", location)
@@ -27,57 +28,60 @@ function ControlPanel({sexMeta, locMeta, yearMeta, chartType}) {
     console.log(newData);
     console.log('just like saying words');
   }
-  
-if(chartType === 'highrates' || chartType === 'lowrates'){
-  return (
-    <>
-    <div className="App">
-      <form onSubmit={dataHandler}>
-      <div className="control">
-        <span className="control__label">Sex: </span>
-        <select id="userSelectedSex" className="select-sex__options">
-          {sexMeta.map((option) => (
-            <option value={`${option.sex_id}`}>{option.sex_short_name}</option>
-          ))}
-        </select>
-      </div>
-      <div className="control">
-        <span className="control__label">Year: </span>
-        <select id="userSelectedYear" className="select-year__options">
-          {yearMeta.map((option) => (
-            <option value={`${option.year_id}`}>{option.year_name}</option>
-          ))}
-        </select>
-      </div>
-      <button type="submit">Hello</button>
-      </form>
-    </div>
-    {chartType === 'highrates' ?<HighRates data={data} /> : <LowRates data={data}/>}
-    
-    {/* <LowRates data={data}/> */}
-    
-    </>
-  );
-}else if(chartType === 'yearly'){
-  return (
-    <>
-    <div className="App">
-      <form onSubmit={dataHandler}>
-      <div className="control">
-        <span className="control__label">Locations: </span>
-        <select id="userSelectedLoc" className="select-loc__options">
-          {locMeta.map((option) => (
-            <option value={`${option.location_id}`}>{option.location_name}</option>
-          ))}
-        </select>
-      </div>
-      <button type="submit">Hello</button>
-      </form>
-    </div>
-    <Yearly data={data}/>
-    </>
-  );
-}
+
+  if (chartType === 'highrates' || chartType === 'lowrates') {
+    return (
+      <>
+        <p className="control-panel-text">Here you can generate data for mortality rates of Opioid Use Disease. <br></br> Choose a Sex and a Year to narrow the scope of the data. <br></br>
+          Navigate Between "Hightest" and "Lowest Mortality Rates by Year" to toggle which limit of the graph you are accessing.
+        </p>
+        <div className="App">
+          <form className='form' onSubmit={dataHandler}>
+            <div className="control">
+              <span className="control__label">Sex: </span>
+              <select id="userSelectedSex" className="select-sex__options">
+                {sexMeta.map((option) => (
+                  <option value={`${option.sex_id}`}>{option.sex_short_name}</option>
+                ))}
+              </select>
+            </div>
+            <div className="control">
+              <span className="control__label">Year: </span>
+              <select id="userSelectedYear" className="select-year__options">
+                {yearMeta.map((option) => (
+                  <option value={`${option.year_id}`}>{option.year_name}</option>
+                ))}
+              </select>
+            </div>
+            <button class="rainbow-button" type="submit">Click to Generate Data</button>
+          </form>
+        </div>
+        {chartType === 'highrates' ? <HighRates data={data} /> : <LowRates data={data} />}
+
+        {/* <LowRates data={data}/> */}
+
+      </>
+    );
+  } else if (chartType === 'yearly') {
+    return (
+      <>
+        <div className="App">
+          <form onSubmit={dataHandler}>
+            <div className="control">
+              <span className="control__label">Locations: </span>
+              <select id="userSelectedLoc" className="select-loc__options">
+                {locMeta.map((option) => (
+                  <option value={`${option.location_id}`}>{option.location_name}</option>
+                ))}
+              </select>
+            </div>
+            <button class="rainbow-button" type="submit">Click to Generate Data</button>
+          </form>
+        </div>
+        <Yearly data={data} />
+      </>
+    );
+  }
 
 }
 
