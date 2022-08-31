de2de4319b2e42010bd1a386993582d11277a312
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-08-31 16:19:57 -0700

    added analysis

--- src/components/LowRatesChart.jsx
+++ src/components/LowRatesChart.jsx
@@ -36,7 +36,10 @@ export default function Chart({ data }) {
         <svg ref={chart}></svg>
         </div>
         <p className="mean-def"> Value Definition of Mean: This is the mean death rate (number of deaths per 100,000 people) due to opioid use disorders for a given location, sex, and year.</p>
-        <p>In the Above Graph the X Axis depicts the Location correlating to the Y axis in which the mean death rate is given.<br></br>Graphs such as this give us a good idea of which geographical locations are most and least affected by Opioid use Disorder.</p>
+        <p>In the Above Graph the X Axis depicts the Location correlating to the Y axis in which the mean death rate is given.</p>
+                <div className='analysis'>
+        <p>If you look at each year's graph you see a trend. In 1990 South Africa has the highest mean death rate due to opioid use disorder, the United States is on the graph, but lower in position(19). <br></br>As you progress through the years graphs, South Africa remains the top hit, but you can also see the United States rates climbing each year.<br></br>By 2003 South Africa is no longer in the top spot, but the United States has climbed to the third place.<br></br>Progressing through the years, in 2009 the United States takes top place, and continued to climb in death rates<br></br>In the most recent year(2017) the United States has climbed to a staggering 14.5 mean death rate, when the second highest location(Libya) has a mean death rate of 7.0.<br></br>The trend that exists through the years clearly shows that the United States started off with a population where opioid use disorder is prevalent, but that prevalence only increased as the years go on.</p>
+        </div>
       </>
     );
   }
