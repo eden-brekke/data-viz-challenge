27e355d87ad4133e32234d33107e9a76a77e568a
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-08-30 21:28:39 -0700

    hard coded citation for style purpose

--- src/components/buildingblocks/Footer.jsx
+++ src/components/buildingblocks/Footer.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import './Footer.css';
 
-export default function Footer({cite}){
+export default function Footer({ cite }) {
   return (
     <>
       <div id='footer'>
-        <a id="footlink" href="http://github.com/eden-brekke">Developed By Eden</a>
-        <p id='cite'> {cite.citation}</p>
+        <a className="dev-link" href="http://github.com/eden-brekke">Developed By Eden</a>
+        <div className="citation-info">
+          <div className='cite-text'>
+            <p>Citation Information: </p>
+            <p className='cite'> GBD Results tool: Use the following to cite data included in this download: Global Burden of Disease Collaborative Network. Global Burden of Disease Study 2017 (GBD 2017) Results.Seattle, United States: Institute for Health Metrics and Evaluation (IHME), 2018.</p>
+          </div>
+          <div className="cite-links">
+            <p>Available from <a className="footer-link" href="http://ghdx.healthdata.org/gbd-results-tool.">GBD Results Tool</a>
+              Permalink to <a className="footer-link" href="http://ghdx.healthdata.org/gbd-results-tool?params=gbd-api-2017-permalink/c12b65baf872c661f4640bb583320b4e">Opioid use Disorder data</a></p>
+          </div>
+        </div>
       </div>
     </>
   )
